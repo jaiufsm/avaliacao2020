2ff3f173998208ee435d0332a1c0214cd45c8937
Felipe-Marin <felipmarin5@gmail.com>
2018-10-21 19:36:12 -0300

    Indicação de melhor e pior opção nas respostas

--- src/interfaces/pergunta.ts
+++ src/interfaces/pergunta.ts
@@ -13,61 +13,61 @@ export class Perguntas{
             "discursiva": false,
             "id": 101,
             "nome": "1) O título do trabalho reflete seu conteúdo e as palavras usadas são adequadas?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 102,
             "nome": "2) O tema do trabalho é relevante na sua respectiva área do conhecimento?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 103,
             "nome": "3) A contextualização do trabalho com a literatura e/ou processos criativos existentes é feita de forma satisfatória?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 104,
             "nome": "4) A metodologia empregada no trabalho reflete o atual estado da arte na sua respectiva área do conhecimento?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 105,
             "nome": "5) Os resultados são apresentados de forma clara, estruturada e coerente, utilizando-se dos meios adequados (tabelas, gráficos, etc.)?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 106,
             "nome": "6) A discussão dos resultados enfatizou seus aspectos mais relevantes e suas limitações?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 107,
             "nome": "7) As conclusões do trabalho são coerentes com seus resultados, métodos e objetivos?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 108,
             "nome": "8) O pôster ou os slides continham as informações necessárias de forma sintética e objetiva?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 109,
             "nome": "9) O apresentador domina o conteúdo do trabalho apresentado?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 110,
             "nome": "10) Qual conceito o(a) Sr(a) daria para o trabalho/apresentador como um todo?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         }                                    
     ];
 
@@ -76,61 +76,61 @@ export class Perguntas{
             "discursiva": false,
             "id": 101,
             "nome": "1) O título do trabalho reflete seu conteúdo e as palavras usadas são adequadas?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 102,
             "nome": "2) O tema do trabalho é relevante na sua respectiva área do conhecimento e/ou apresenta possibilidade de trabalho interdisciplinar com outras áreas?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 103,
             "nome": "3) A proposta do trabalho compromete-se com as demandas da sociedade, promovendo intercâmbio de experiências e saberes entre Universidade e demais setores da sociedade?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 104,
             "nome": "4) As ações realizadas contribuem com: inclusão de grupos sociais; desenvolvimento de meios e processos de produção; inovação e construção de conhecimento; ou à ampliação de oportunidades educacionais e formativas?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 105,
             "nome": "5) Os resultados são apresentados de forma clara, estruturada e coerente, utilizando-se dos meios adequados?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 106,
             "nome": "6) A sistematização dos resultados enfatizou seus aspectos mais relevantes e suas limitações?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 107,
             "nome": "7) As conclusões do trabalho são coerentes com seus objetivos, ações e resultados?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 108,
             "nome": "8) O pôster ou os slides continham as informações necessárias de forma sintética e objetiva?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 109,
             "nome": "9) O apresentador domina o conteúdo do trabalho apresentado?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         },
         {
             "discursiva": false,
             "id": 110,
             "nome": "10) Qual nota o(a) Sr(a) daria para o trabalho/apresentador como um todo?",
-            "respostas": "A;B;C;D;E"
+            "respostas": "A - Melhor;B;C;D;E - Pior"
         }                                    
     ]
 }
